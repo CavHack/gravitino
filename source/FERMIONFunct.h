bf72e7d1cecc2178a2f93bef6774b4a34d4ac6bb
Karl Whitford <karlwhitford@v-l.live>
2018-12-12 22:04:24 -0500

    First Writer to interact with rna-seq
    A chunk of RNA splice transcriptome loci are inferred as a set of indexed prefixes.

---
+++ source/FERMIONFunct.h
@@ -0,0 +1,9 @@
+#ifndef DEF_FERMIONFunct
+#define DEF_FERMIONFunct
+
+#include SAM_BRUTUS_H
+
+void outFERMIONWriteHeader(*BRUTUS fp, const string &samh, const vector<string &chrn, const vector <uint> &chrl);
+int brutusReadFromNet(char *brutusChar, brut1_t *b);
+string brutus_cigarstring(brut1_t *b);
+#endif
