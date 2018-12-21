51d83c11a5f3ae7fe3bbfd6ec03234036a4f672e
Karl Whitford <karlwhitford@v-l.live>
2018-12-20 20:33:43 -0500

    setting up GenomeQueue
    first steps

--- source/GenomeQueue.h
+++ source/GenomeQueue.h
@@ -29,9 +29,46 @@ class GenomeQueue {
   //genome length, SA lenght, # of chromosomes
   uint nGenomeStack, nSAbytes, nChrReal;
   uint nGenomeStack2, nSA2, nSAbyte2, nChrReal2; 
-  uint nSAil // SA index.
+  uint nSAi; // SA index.
+
+  unsigned char GrstrandBit, SAiMarkNbit, SAiMarkAbsentBit; //SA index bit for strand information
+  uint GstrandMask, SAiMarkAbsentMask, SAiMarkAbsentMaskC, SAiMarkNmask, SAiMarkNmaskC; //remove strand bit from index.
+
+  //SJ database parameters
+  uint spliceJDBOverhang, spliceJDBLength; //length of the donor/acceptor, lenght of the splice junction "chromosome" = 2 * 
+  uint spliceJChrStart, spliceJDBN; //first sj-db chr
+  uint spliceJGstart; // start of the sj-db genome sequence
+  uint *spliceJDstart, *spliceJAstart, *spliceJDBStart, *spliceJDBEnd; //sjdb loci
+  uint8 *spliceJDBMotif; //motifs of annotated junctions
+  uint8 *spliceJDBShiftLeft, *spliceJDBShiftRight; //shift of junctions
+  uint8 *spliceJDBStrand; // Junctions strand, not used yet.
+
+  //sequence insert parameters
+  uint genomeInsertL; //total length of the sequence to be intserted on the fly.
+  uint genomeInsertChrIndFirst; //index of the first inserted chromosome.
+
+  sharedMemory * sharedMemory;
+
+  GenomeQueue (Params &Pin);
+  //~GenomeQueue();
+
+  void freeMemory();
+  void genomeLoad();
+  void chrBinFill();
+  void chrInfoLoad();
+
+  void insertSequences();
+  void genomeGenerate();
+
+  private:
+    Params &P;
+    key_t shmKey;
+    char *shmStart;
+    char *G1;
+    uint OpenStream(string name, ifstream & stream, uint size);
+    void HandleSharedMemoryExceptions(const SharedMemoryException & exc, uint64 shmSize);
 
 
+};
 
-
-}
+#endif
