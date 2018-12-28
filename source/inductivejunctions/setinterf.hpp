5391c286637775b1651c78031e0ed3d1314c8c9d
Karl Whitford <karlwhitford@v-l.live>
2018-12-27 20:07:56 -0500

    Laying the groundwork
    to build a prover which infers matroid indices embedded in an interactive arithmetic intermediate ricci flow.

---
+++ source/inductivejunctions/setinterf.hpp
@@ -0,0 +1,44 @@
+#include "JosephsonJunctions.hpp"
+#include <rings/LaplacianOperators.hpp>
+
+#include <memory>
+
+#ifndef __SETINTERF_H__
+#define __SETINTERF_H__
+
+namespace Ring{
+
+
+  class setinterf {
+  public:
+    /**
+     * @brief Returns '1' (boolean true) if it exists a predicate which satisfies the condition.
+     * @param make a prediction on the predicate
+     * @return True iff an element that satisfies the predicate exists in the set
+     */
+    virtual bool exist(const std::unique_ptr<const JosephsonJunction>& pred) const = 0;
+
+    /*
+     * Return size of the finite set in question
+     *
+     */
+
+    virtual size_t size() const  = 0;
+
+    /*
+     *Returns whether a field element is a member of the set
+     *
+     */
+
+    virtual bool contains(const JosephsonJunction& e) const = 0;
+
+    virtual ~setinterf(){};
+
+
+  };
+
+
+
+} //namespace ring
+
+#endif // __SETINTERF_H__
